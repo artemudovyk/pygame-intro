d4e256f6b0e5563bdde47ebb97e3eacd9fae3a02
Artem Udovyk <udovyk@gmail.com>
2023-09-16 14:18:00 +0300

    feat: add assets and surfaces

--- src/main.py
+++ src/main.py
@@ -7,7 +7,18 @@ import settings
 pygame.init()
 
 # Create the screen
-screen = pygame.display.set_mode((settings.WINDOW_WIDTH, settings.WINDOW_HEIGHT))
+screen = pygame.display.set_mode(size=(settings.WINDOW_WIDTH, settings.WINDOW_HEIGHT))
+# Set the title
+pygame.display.set_caption(settings.GAME_NAME)
+# Init the clock
+clock = pygame.time.Clock()
+
+test_font = pygame.font.Font("./assets/fonts/Pixeltype.ttf", 50)
+
+# Create surfaces
+sky_surface = pygame.image.load("./assets/images/sky.png")
+ground_surface = pygame.image.load("./assets/images/ground.png")
+score_surface = test_font.render("My Game", False, "black")
 
 while True:
     # Handle events
@@ -16,5 +27,10 @@ while True:
             pygame.quit()
             sys.exit()
 
+    screen.blit(sky_surface, dest=(0, 0))
+    screen.blit(ground_surface, dest=(0, 300))
+    screen.blit(score_surface, dest=(350, 50))
+
     # Update the screen
     pygame.display.update()
+    clock.tick(settings.MAX_FPS)
